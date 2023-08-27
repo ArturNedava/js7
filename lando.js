60a437dc0bc4ee495be063e4df7ecdcd6991eb1a
ArturNedava <121451856+ArturNedava@users.noreply.github.com>
2023-08-27 14:59:21 +0200

    First commit

---
+++ lando.js
@@ -0,0 +1,75 @@
+
+//let array = [1, 2, 3]
+
+//array[2] = 10;
+
+//console.log(array);
+
+
+
+//let fruits = ["apple", "banana", "pineapple"]
+
+//fruits.push("fruikt")
+
+//console.log(fruits)
+
+
+
+
+//let num = [5, 4, 3, 2, 1];
+
+//let sum = 0;
+
+//for (let i = 0; i < num.length; i++) {
+  //  sum += num[i];
+  //}
+  
+  //console.log("Сума чисел в масиве: " + sum);
+
+
+
+
+//let number = [1, 2, 3, 4, 5]
+
+//for(let i = 0; i < number.length; i++){
+ //  console.log(number[i]);
+//}
+
+
+
+
+//let wordss = ["houses", "book", "sky", "game", "vocabulary"]
+
+//for(let i = 0; i < wordss.length; i++){
+  //  if(wordss[i].length > 5){
+  //      console.log(wordss[i]);
+  //  }
+//}
+
+
+
+
+//let namber = [1, 3, 5, 8, 19, 32, 54, 300, 34, 45];
+
+//let max = namber[0];
+
+//for(let i = 1; i < namber.length; i++){
+ //   if(namber[i] > max){
+  //      max = namber[i];
+  //  }
+//}
+//console.log("Максимальное число:" + max);
+
+
+
+
+//let nambers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+//for(let i = 0; i < nambers.length; i++){
+  //  if(nambers[i] % 2 === 0){
+  //      console.log(nambers[i]);
+  //  }
+//}
+
+//console.log("Gарные числа в масиве:")
+
